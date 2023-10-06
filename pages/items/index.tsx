c997984957f60b6b322e8c874fbe398293d78617
legaspicary <legaspicary@gmail.com>
2023-10-06 21:43:01 +0800

    feat: add placeholder for display items

--- pages/items/index.tsx
+++ pages/items/index.tsx
@@ -1,34 +1,97 @@
 "use client";
 
-import { zodResolver } from "@hookform/resolvers/zod";
-import { useForm } from "react-hook-form";
-import * as z from "zod";
-
+import Image from "next/image";
 import { Button } from "@/components/ui/button";
-import {
-  Form,
-  FormControl,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
-} from "@/components/ui/form";
-import { Input } from "@/components/ui/input";
-import { toast } from "@/components/ui/use-toast";
-import Link from "next/link";
-import { Card } from "@/components/ui/card";
+import { useState } from "react";
 
-const FormSchema = z.object({
-  username: z.string().min(2, {
-    message: "Username must be at least 2 characters.",
-  }),
-  password: z.string(),
-});
+type Category = {
+  name: string;
+  selected: boolean;
+};
+
+type Item = {
+  name: string;
+  description: string;
+  qty: number;
+  unit: string;
+};
 
 export default function Items() {
+  const [categoriesList, setCategoriesList] = useState<Category[]>(
+    Array(5)
+      .fill({
+        name: "Item Filter",
+        selected: false,
+      })
+      .map((cat, i) => ({ ...cat, name: `${cat.name} ${i + 1}` }))
+  );
+  const itemsList: Item[] = Array<Item>(50)
+    .fill({
+      name: "My Item",
+      description: "The Item Description is a very important part of an item",
+      qty: 5,
+      unit: "pcs.",
+    })
+    .map((item, i) => {
+      const qty = i % 2 === 0 ? 5 + i : 5 - i;
+      return { ...item, qty, unit: qty > 1 ? "pcs." : "pc." };
+    });
+
+  const onCategorySelect = (i: number, category: Category) => {
+    categoriesList[i] = { ...category, selected: !category.selected };
+    setCategoriesList([...categoriesList]);
+  };
+
   return (
-    <div className="h-full w-screen bg-slate-500">
-      <h1>Hello</h1>
-    </div>
+    <section className="h-full grid grid-cols-5 gap-4">
+      <div>
+        <h3 className="font-semibold">Categories</h3>
+        <ul>
+          {categoriesList.map((category, i) => (
+            <li key={i} className="pt-1">
+              <Button
+                className="h-15 w-full py-1 justify-start"
+                variant={category.selected ? "default" : "secondary"}
+                onClick={() => onCategorySelect(i, category)}
+              >
+                {category.name}
+              </Button>
+            </li>
+          ))}
+        </ul>
+      </div>
+      <section className="col-span-4">
+        <h3 className="pb-1 font-semibold">Items</h3>
+        <div className="h-full grid grid-cols-4 gap-4">
+          {itemsList.map((item, i) => (
+            <div className="bg-slate-50 h-96" key={i}>
+              <Image
+                src={"/rickroll_4k.jpg"}
+                height={0}
+                width={0}
+                sizes="100vw"
+                style={{ width: "100%", height: "auto" }}
+                alt={"rickrolls"}
+              />
+              <h5 className="px-2 mt-2">
+                {item.name}
+                <span
+                  className={`ml-2 ${
+                    item.qty > 5 ? "text-slate-600" : "text-red-500"
+                  } text-xs`}
+                >
+                  {item.qty}
+                  {item.unit}
+                </span>
+                {item.qty <= 5 && (
+                  <span className="ml-1 text-red-500 text-xs">left</span>
+                )}
+              </h5>
+              <p className="px-2 mt-2 text-xs">{item.description}</p>
+            </div>
+          ))}
+        </div>
+      </section>
+    </section>
   );
 }

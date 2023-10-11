fb3335d367c9cb3ad961eadbdb9cd08f825fe8d0
legaspicary <legaspicary@gmail.com>
2023-10-11 20:52:41 +0800

    add app title in nav

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,6 +1,7 @@
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
 import { signOut, useSession } from "next-auth/react";
+import { LeafyGreen } from "lucide-react";
 
 const Nav = () => {
   const { data: session } = useSession();
@@ -8,7 +9,17 @@ const Nav = () => {
   return (
     <div className="bg-zinc-100 py-2 border-b border-s-zinc-200 fixed w-full z-10 top-0">
       <div className="container flex items-center justify-between">
-        <Link href="/">Logo</Link>
+        <Link href="/">
+          <h1 className="text-xl font-medium">
+            <LeafyGreen
+              color="#1F8A70"
+              strokeWidth={3}
+              size={18}
+              className="inline"
+            />{" "}
+            cabbage.io
+          </h1>
+        </Link>
 
         {session?.user ? (
           <Button
